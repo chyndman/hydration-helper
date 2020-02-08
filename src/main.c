7fa4b7ad75e12e0d27da87639a3c95a4148476a7
Chris Hyndman <chrishyndman@outlook.com>
2020-02-08 10:58:46 -0800

    main different demo

--- src/main.c
+++ src/main.c
@@ -2,19 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include <stdio.h>
-#include <time.h>
 #include "dev/rdb-leds.h"
 #include "dev/sparkfun-qwiic-twist.h"
 #include <applibs/log.h>
 
-int main(void) {
+int main(void)
+{
     Log_Debug("Hello, world!");
     while (1)
     {
-        // devSparkFunQwiicTwistGetCount()
-        Log_Debug("Count %i", devSparkFunQwiicTwistGetCount());
-        const struct timespec sleepTime = {1, 0};
-        nanosleep(&sleepTime, NULL);
+        int16_t count = devSparkFunQwiicTwistGetCount();
+        devRdbLedSetColor(RDBLED_USER1, (uint16_t)count & 0x7);
+        devRdbLedSetColor(RDBLED_USER2, ((uint16_t)count >> 3) & 0x7);
+        devRdbLedSetColor(RDBLED_USER3, ((uint16_t)count >> 6) & 0x7);
+        devRdbLedSetColor(RDBLED_USER4, ((uint16_t)count >> 9) & 0x7);
     }
 }
