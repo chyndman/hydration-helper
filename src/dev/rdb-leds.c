fa4a399d81d7ff61b0fb4e4f343b2d5c399afd1d
Chris Hyndman <chrishyndman@outlook.com>
2020-08-31 23:32:40 -0700

    switch to bsd 3-clause license

--- src/dev/rdb-leds.c
+++ src/dev/rdb-leds.c
@@ -1,7 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 #include "rdb-leds.h"
 #include <applibs/gpio.h>
 #include <hw/mt3620_rdb.h>
--- src/pomodoro.c
+++ src/pomodoro.c
@@ -1,7 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 #include "pomodoro.h"
 #include "light-ctrl.h"
 #include "dev/sparkfun-qwiic-twist.h"
